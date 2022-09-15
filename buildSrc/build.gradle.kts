4b42868aa5a727df1f847fefdc77e27d6009f27b
tuanwan <tuanwan@pateo.com.cn>
2022-09-15 14:59:35 +0800

    kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,12 +17,7 @@ repositories {
     maven(url = "https://jitpack.io")
 }
 dependencies {
-    // in order to be able to use "kotlin-android" in the common script
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-
-    // in order to recognize the "plugins" block in the common script
     implementation("com.android.tools.build:gradle:7.2.2")
-
-    // in order to recognize the "android" block in the common script
     implementation("com.android.tools.build:gradle-api:7.2.2")
 }
